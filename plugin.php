b44631d66e90a99b9770d78f9031fc35dc4e692f
Arsenii Yamnii <yamnii@ya.ru>
2021-03-30 02:45:32 +0500

    refactor: edit variables names
    Signed-Off-By Arsenii Yamnii
    yamnii@ya.ru

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
    /*
-   Plugin Name: Awesomeness Creator
+   Plugin Name: Awesomeness Creator2
    Plugin URI: http://my-awesomeness-emporium.com
    description: >-
   a plugin to create awesomeness and spread joy
@@ -9,15 +9,15 @@
    Author URI: http://mrtotallyawesome.com
    License: GPL2
    */
-function m100-block() {
+function m100block() {
     wp_register_script(
-        'guten-tuts',
+        'm100-block',
         plugins_url( 'js/block.js', __FILE__ ),
         array( 'wp-blocks', 'wp-element', 'wp-i18n' )
     );
  
-    register_block_type( 'guten-tuts/alert', array(
-        'editor_script' => 'guten-tuts',
+    register_block_type( 'yamnish/m100', array(
+        'editor_script' => 'm100-block',
     ) );
 }
-add_action( 'init', 'm100-block' );
+add_action( 'init', 'm100block' );
