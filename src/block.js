46025df48f8bd32edd2e4c6333b05ffaf8aa2883
Arsenii Yamnii <yamnii@ya.ru>
2021-03-30 15:08:31 +0500

    fix: add my classic block as boilerplate
    Signed-Off-By Arsenii Yamnii
    yamnii@ya.ru

--- src/block.js
+++ src/block.js
@@ -1,48 +1,110 @@
-var Vel = wp.element.createElement,
-    __ = wp.i18n.__,
-    registerBlockType = wp.blocks.registerBlockType,
-    RichText = wp.blocks.RichText,
-    blockStyle = { backgroundColor: '#900', color: '#fff', padding: '20px' };
+const { registerBlockType } = wp.blocks;
+const {
+    RichText,
+    InspectorControls,
+    ColorPalette,
+    MediaUpload
+} = wp.editor;
+const {
+    PanelBody,
+    IconButton,
+    RangeControl
+} = wp.components;
 
-registerBlockType( 'yamnish/m100', {
-
-    title: __( 'M100' ),
-
-    icon: 'megaphone',
-
+registerBlockType('yamnish/m100', {
+    title: 'Two column text',
+    description: 'A25 Bootstrap block with two column text',
+    icon: 'format-image',
     category: 'layout',
 
+    // custom attributes
     attributes: {
-        text : {
-            type: 'array',
-            source: 'children',
-            selector: 'div',
-        }
+        title: {
+            type: 'string',
+        },
+        body: {
+            type: 'string',
+        },
+        title2: {
+            type: 'string',
+        },
+        body2: {
+            type: 'string',
+        },
     },
 
-    edit: function( props ) {
-        var content = props.attributes.text;
+    edit({ attributes, setAttributes }) {
+        const {
+            title,
+            body,
+            title2,
+            body2,
+        } = attributes;
 
-        function onChangeText( newText ) {
-            props.setAttributes( { text: newText } );
+        // custom functions
+        function onChangeTitle(newTitle) {
+            setAttributes( { title: newTitle } );
         }
 
-        return el(
-            RichText,
-            {
-                tagName: 'div',
-                className: props.className,
-                onChange: onChangeText,
-                value: content,
-                isSelected: props.isSelected,
-                style: blockStyle
-            }
+        function onChangeBody(newBody) {
+            setAttributes( { body: newBody } );
+        }
+        function onChangeTitle2(newTitle) {
+            setAttributes( { title2: newTitle } );
+        }
+
+        function onChangeBody2(newBody) {
+            setAttributes( { body2: newBody } );
+        }
+
+
+        return ([
+            <div>
+            <RichText key="editable"
+            tagName="h2"
+            placeholder="Заголовок"
+            value={ title }
+            onChange={ onChangeTitle }/>
+            <RichText key="editable"
+            tagName="p"
+            placeholder="Текст"
+            value={ body }
+            onChange={ onChangeBody }/>
+            <RichText key="editable"
+            tagName="h2"
+            placeholder="Заголовок2"
+            value={ title2 }
+            onChange={ onChangeTitle2 }/>
+            <RichText key="editable"
+            tagName="p"
+            placeholder="Текст2"
+            value={ body2 }
+            onChange={ onChangeBody2 }/>
+            </div>
+        ]);
+    },
+
+    save({ attributes }) {
+        const {
+            title,
+            body,
+            title2,
+            body2,
+
+        } = attributes;
+
+        return (
+            <div class="row">
+            <div class="col col-lg-6 col-md-12 box justify-content-center">
+            <RichText.Content tagName="h2" value={ title }/>
+            <RichText.Content tagName="p" value={ body }/>
+            </div>
+            <div class="col col-lg-6 col-md-12 box justify-content-center">
+            <RichText.Content tagName="h2" value={ title2 }/>
+            <RichText.Content tagName="p" value={ body2 }/>
+            </div>
+            </div>
         );
-    },
+    }
+});
 
-    save: function( props ) {
-        var text = props.attributes.text;
-
-        return el( 'div', { style: blockStyle, className: props.className }, text );
-    },
-} );
